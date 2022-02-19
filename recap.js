a1a0c35f3a390d5980617d534cad05c2ee94a4fa
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-02-20 00:33:02 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,6 @@
+const hubby = 'Sani';
+let phone=`samsung`
+
+// template string
+const myNotes = `My Favorite actor is ${hubby}.No one actor I like.I have a ${phone} mobile.`;
+console.log(myNotes);
